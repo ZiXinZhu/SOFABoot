7779a2e932a27fd453b0ce380be1d40f86a85119
husky <1101648204@qq.com>
2020-03-20 22:43:47 +0800

    SOFABoot

---
+++ src/main/java/com/zzx/sofaboots/SofabootsApplication.java
@@ -0,0 +1,13 @@
+package com.zzx.sofaboots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofabootsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SofabootsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzx/sofaboots/SofabootsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zzx.sofaboots;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofabootsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
